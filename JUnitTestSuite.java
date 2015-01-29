b989aa5f09e21e85c4fe2266a676114ab33826a4
larrynet <lawrenceng3@hotmail.com>
2015-01-29 00:31:45 -0500

    Add template JUnit

---
+++ JUnitTestSuite.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JUnitTestSuite {
+
+	/**
+	 * Test will test if number of components are valid and handle the case
+	 */
+	@Test
+	public void TestGameEngineComponents() {
+	    //TBT: 	ImportState and ExportState
+		
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test player
+	 */
+	@Test
+	public void TestBoardCompoents() {
+		//first load a good state
+		//test afterward
+		//generate fails
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
