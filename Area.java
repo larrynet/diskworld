36557844b4f6df8edd04efdad54418d3dc3ae8b2
pari naz <parinaz1983@yahoo.com>
2015-01-29 16:03:28 -0500

    SaveState() Added

--- Area.java
+++ Area.java
@@ -1,14 +1,13 @@
-
-
-/**
- * @author pari Alt+shift+j
- *
- */
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * @author pari
+ *
+ */
 public class Area 
 {
+	    //Attributes
 		private String[] ArrName ={ "Dolly Sisters","Unreal Estate","Dragon's Landing","Small Gods","The Scours","The Hippo","The Shades","Dimwell","Longwall","Isle of Gods","Seven Sleepers","Nap Hill"};
 		private String Name;
 		private int[] ArrBuildingCost={6,18,12,18,6,12,6,6,12,12,18,12};
@@ -16,55 +15,95 @@ public class Area
 		private int Number;
 		public  boolean IsEmpty;
 		public boolean IsBuilt; //in case a building is built in the area
-		public List<Pieces> TroubleMakers;
-		public List<Pieces> Demons;
-		public List<Pieces> Trolls;
+		public boolean IsTrouble; //in case two minions are in the area
+		public List<Pieces> TroubleMakers=new ArrayList<Pieces>();
+		public List<Pieces> Demons=new ArrayList<Pieces>();
+		public List<Pieces> Trolls=new ArrayList<Pieces>();
+		public List<Pieces> Minions=new ArrayList<Pieces>();
+		private int[][] AdjacentAreas=new int[][]
+				{
+					//first numbers is the number of Area,0 is there to keep the number of each column to 7
+					//an array of 12*7
+					{1,2,3,12,0,0,0},
+					{2,1,4,11,10,0,0},
+					{3,1,4,2,0,0,0},
+					{4,3,6,5,0,0,0},
+					{5,4,6,7,10,8,0},
+					{6,4,5,7,0,0,0},
+					{7,6,5,8,0,0,0},
+					{8,9,7,5,0,0,0},
+					{9,8,11,10,0,0,0},
+					{10,2,4,5,9,11,0},
+					{11,9,12,2,10,0,0},
+					{12,11,1,2,0,0,0}
+					
+				};
+		
+		
+		//Methods
 		public String GetName() {return Name;}
 		
-		public Area(boolean b) {}
+		public Area(boolean b) //what is this? what is that boolean?
+		{
+			
+		}
+		
 		public void AddDemons(Pieces p) 
 		{
 			Demons.add(p);
 		}
+		
 		public void AddTrolls(Pieces p) 
 		{
 			Trolls.add(p);
 		}
-		public void AddTroubleMaker(Pieces p) 
+		
+		public void AddTroubleMaker(Pieces p) //only onetroublemarker in each area can be--we should check the number
 		{
 			TroubleMakers.add(p);
+			this.IsTrouble =true;
+		}
+		public void AddMinions(Pieces p) 
+		{
+			Minions.add(p);
 		}
 		
-		private int[][] AdjacentAreas=new int[][]
+		
+		public void RemoveDemons(Pieces p) 
 		{
-			//first numbers is the number of Area,0 is there to keep the number of each column to 7
-			//an array of 12*7
-			{1,2,3,12,0,0,0},
-			{2,1,4,11,10,0,0},
-			{3,1,4,2,0,0,0},
-			{4,3,6,5,0,0,0},
-			{5,4,6,7,10,8,0},
-			{6,4,5,7,0,0,0},
-			{7,6,5,8,0,0,0},
-			{8,9,7,5,0,0,0},
-			{9,8,11,10,0,0,0},
-			{10,2,4,5,9,11,0},
-			{11,9,12,2,10,0,0},
-			{12,11,1,2,0,0,0}
+			Demons.remove(p);
+		}
+		
+		public void RemoveTrolls(Pieces p) 
+		{
+			Trolls.remove(p);
+		}
+		
+		public void RemoveTroubleMaker(Pieces p) 
+		{
+			TroubleMakers.remove(p);
+			this.IsTrouble=false ;
 			
-		};
+		}
+		public void RemoveMinions(Pieces p) 
+		{
+			Minions.remove(p);
+		}
+		
+		
 		
 		 
 		//first initialization 
 		public Area()
-	 	{
+	 	{System.out.println( "Number"+"  "+ "Name"+"  "+" Minions"+"   $"+ "BuildingCost" +"  "+"Empty?"+"  "+"Trouble?"+"  "+"Building?"+"  "+"Demons"+"  "+"Trolls");
 		for (int i=0;i<12;i++)
 		{
 		    this.Name=ArrName[i];
 		    this.BuildingCost=ArrBuildingCost[i];
 		    this.Number=i+1;
+		    this.IsEmpty =true;
 		    
-		    switch(Number)
+		  /*  switch(Number)
 		    {
 		    case 1:
 		    case 5:
@@ -80,20 +119,21 @@ public class Area
 		    	}
 		    		
 		     }
+		     */
 		    
 		    this.IsBuilt=false;
 		    
 		    System.out.println();
 		    System.out.println();
-		    System.out.println("Area" + (Number)+"  "+ Name+ "   $"+ BuildingCost  +" is created  "
-	    			 +IsEmpty +"  "+IsBuilt);
+		    System.out.println( Number+"  "+ Name+"  "+ Minions+"   $"+ BuildingCost +"  "+ IsEmpty +"  "+IsTrouble+"  "+IsBuilt+"  "+Demons+"  "+Trolls);
+
 		    System.out.print("Adjacent areas for Area" + (i+1) + "  are  ");
 		    
 		     for (int j=0;j<7;j++)
-		     {
-		    	 
-		    	 System.out.print( AdjacentAreas[i][j] + "  ");
-		     }
+		     {	
+		    	 System.out.print( AdjacentAreas[i][j] + "  ");//we do not have area 0.0 means that adjacent areas finished.
+		     		    	 
+		    	 }
 		}
 		 System.out.println();
 		 System.out.println();
@@ -105,11 +145,12 @@ public class Area
 		//set Variables what info lawrence should send for me to reload
 		//IsEmpty is not enough.we should know which player.which piece.is in the area
 		//IsBuilt is not enough.Which player has built in the area?
-		public Area(int Number, boolean IsEmpty, boolean IsBuilt) 
+		public Area(int Number, boolean IsTrouble,boolean IsEmpty, boolean IsBuilt) 
 		{
 			this.Number=Number;
 			this.IsEmpty=IsEmpty;
 			this.IsBuilt=IsBuilt;
+			this.IsTrouble=IsTrouble;
 			this.Name=ArrName[Number-1];
 			this.BuildingCost=ArrBuildingCost[Number-1];
 			
@@ -119,7 +160,7 @@ public class Area
 		}
 		
 		//restore from saved point---set Area
-		public void CreateArea(int Number, boolean IsEmpty, boolean IsBuilt)
+		public void CreateArea(int Number,  boolean IsTrouble,boolean IsEmpty, boolean IsBuilt,List<Pieces> Minions,List<Pieces> Trolls,List<Pieces> Demons,List<Pieces> TroubleMaker)
 		{
 			this.Number=Number;
 			this.IsEmpty=IsEmpty;
@@ -132,42 +173,102 @@ public class Area
 		}
 		
 		
-		public void GetArea()
+		
+		public String GetAreaState()
 		{
+			System.out.println("current state of Area");
+			System.out.println( Number +"  "+ Name+"  "+"Minions   Trouble?    Building? "+"   $"+ BuildingCost+"Demons   Trolls");
+			
 			for (int i=0;i<12;i++)
 			{
 				
 				
-			System.out.println("Area" + (Number)+"  "+ Name+ "   $"+ BuildingCost  +" is created  " +
-					IsEmpty +"  "+IsBuilt);
+			System.out.println( Number +"  "+ Name+"  "+"Minions   Trouble?    Building? "+"   $"+ BuildingCost+"Demons   Trolls");
 			}
-		}
+			return "";
 		
-	/*	public String SaveArea()
+		
+		}
+		public String SaveArea()
 		{
 			//Area,..........,Last
 			// Each Area should be saved
 			//Format of output should be passed to lawrence
+			 String EachArea;
+			 String AllAreas="";
+			 for (int i=0;i<12;i++)
+			 {
+				 EachArea=( "Area,"+(i+1)+","+ ArrName[i]+","+ Minions+","+ IsEmpty +","+IsTrouble+","+IsBuilt+","+Demons+","+Trolls+",last \n");
+                 //System.out.println(EachArea);
+                 
+				 AllAreas=AllAreas.concat(EachArea);
+				 
+			 }
+             //System.out.println(AllAreas);
+
+				 return AllAreas;
+			 
 		}
+			 
+}	 
 		
-		public void print() //to print for demo
+		
+	/*	public void print() //to print for demo
 		{
 			
 		}
 		
 		// to define if two area are adjacent
-		public boolean isAdjacent(int playerNumber,int areaNumber)
-		{
-			public int
-		}
+	//	public boolean isAdjacent(int playerNumber,int areaNumber)
+	//	{
+	//		public int
+	//	}
 		
 		
-		SaveState(BufferedWriter bw)
-		{
-		}
-		*/
+		//SaveState(BufferedWriter bw)
+		//{
+	//	}
 		
 }
+		
+		*/
+		
+		/*
+		 * public int GetMoneyCount()
+	{
+		return this.Money;
+	}
+	
+	/*
+	 * Add to player bank roll
+	 * 
+	 * @param amount to add
+	 */
+	
+	
+	/*
+	 * Deduct from player bank roll
+	 * 
+	 * @param amount to deduct
+	 */
+	
+
+	/*
+	 * Get the current state of the player as a string
+	 * 
+	 * @return Current state
+	 */
+	//public String GetPlayerState()
+//	{
+		/*
+		String playerState = "Number;"+Integer.toString(this.PlayerNumber) + 
+							 "personality"+Integer.toString(this.Personality.GetCardID());
+							 
+		return playerState; */
+	//	return "";
+	//}
+		 
+//}
 	
 	
 	
