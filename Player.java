a94c951437fc5cd221e7ace68df718afc36a1114
Gay Hazan <gay.hazan@gmail.com>
2015-01-31 19:20:09 -0500

    Player Profile and Status

--- Player.java
+++ Player.java
@@ -102,6 +102,8 @@ public class Player implements Serializable{
 	}
 	
 	public List<Cards> GetCards() {return this.PlayerCards;}
+	
+	
 	/**
 	 * Get the count of minions
 	 * @return number of minions currently belonging to user
@@ -120,6 +122,11 @@ public class Player implements Serializable{
 		return this.ListBuildings.size();
 	}
 	
+	public void PrintPlayerProfile()
+	{
+		System.out.println("Player " + this.PlayerNumber + "(" + this.Color + ")" + "is playing as " + this.Personality.Name + ".");
+
+	}
 	
 	
 	/**
@@ -128,23 +135,11 @@ public class Player implements Serializable{
 	 * @return Player State format : Player, player number, personality card id, money count, number of cards,_
 	 * cards ids, number of minions, minion ids, number of buildings, buildings ids, last
 	 */
-	/*public String GetPlayerState()
+	public void GetPlayerState()
 	{
-		
-		String playerState = 	"Player,"
-								+ Integer.toString(this.PlayerNumber) + "," 
-								+ "personality"+Integer.toString(this.Personality.GetCardID()) + ","
-								+ Integer.toString(this.GetMoneyCount()) + ","
-								+ Integer.toString(this.GetPlayerCardCount()) + ","
-								+ this.GetPlayerCardIds() + ","
-								+ Integer.toString(this.GetMinionCount()) + ","
-								+ this.GetMinionIds() + ","
-								+ Integer.toString(this.GetBuildingCount()) + ","
-								+ this.GetBuildingIds() + ","
-								+ "last";
-							 
-		return playerState;
-	}*/
+		System.out.println("Player " + this.PlayerNumber + "'s current invetory");
+		System.out.println(this.GetMinionCount() + " minions, " + this.GetBuildingCount() + " buildings " + this.GetMoneyCount() + " Ankh-Morpork dollars");
+	}
 	
 	
 	/**
@@ -156,75 +151,71 @@ public class Player implements Serializable{
 		return Color;
 	}
 	
-		/**
-		 * Iterate through the player cards and create a string of card id separated by a comma
-		 * @return PlayerCards Id's in comma delimited format
-		 */
-		private String GetPlayerCardIds()
+	/**
+	 * Iterate through the player cards and create a string of card id separated by a comma
+	 * @return PlayerCards Id's in comma delimited format
+	 */
+	private String GetPlayerCardIds()
+{
+	StringBuilder AllCards = new StringBuilder();
+	
+	//
+	/*for (int i = 0; i < this.GetPlayerCardCount(); i++)
 	{
-		StringBuilder AllCards = new StringBuilder();
+		AllCards.append(Integer.toString(this.PlayerCards.get(i).GetCardID()));
 		
-		//
-		/*for (int i = 0; i < this.GetPlayerCardCount(); i++)
+		//Add Comma logic
+		if (i < this.GetPlayerCardCount())
 		{
-			AllCards.append(Integer.toString(this.PlayerCards.get(i).GetCardID()));
-			
-			//Add Comma logic
-			if (i < this.GetPlayerCardCount())
+			AllCards.append(",");
+		}
+	}
+	*/
+	return AllCards.toString();
+}
+
+
+	public void RetrieveMinion(Pieces Minion)
+	{
+		if (this.GetMinionCount() < 12)
+		{
+			if (Minion.GetPieceColor() == this.Color && Minion.GetPieceType() == PieceType.Minion)
 			{
-				AllCards.append(",");
+				this.ListMinions.add(Minion);
 			}
 		}
-		*/
-		return AllCards.toString();
 	}
 	
+	public Pieces PlaceMinion()
+	{
+		Pieces MinionToSend =  this.ListMinions.get(this.GetMinionCount()-1);
+			
+		this.ListMinions.remove(this.GetMinionCount()-1);
+		
+		
+		return MinionToSend;
 	
-		public void RetrieveMinion(Pieces Minion)
+	}
+	
+	public void RetrieveBuiding(Pieces Building)
+	{
+		if (this.GetMinionCount() < 6)
 		{
-			if (this.GetMinionCount() < 12)
+			if (Building.GetPieceColor() == this.Color && Building.GetPieceType() == PieceType.Building)
 			{
-				if (Minion.GetPieceColor() == this.Color && Minion.GetPieceType() == PieceType.Minion)
-				{
-					this.ListMinions.add(Minion);
-				}
+				this.ListBuildings.add(Building);
 			}
 		}
-		
-		public Pieces PlaceMinion()
-		{
-			Pieces MinionToSend =  this.ListMinions.get(this.GetMinionCount()-1);
-				
-			this.ListMinions.remove(this.GetMinionCount()-1);
-			
-			
-			return MinionToSend;
-		
-		}
-		
-		public void RetrieveBuiding(Pieces Building)
-		{
-			if (this.GetMinionCount() < 6)
-			{
-				if (Building.GetPieceColor() == this.Color && Building.GetPieceType() == PieceType.Building)
-				{
-					this.ListBuildings.add(Building);
-				}
-			}
-		}
-		
-		public Pieces PlaceBuilding()
-		{
-			Pieces BuildingToSend =  this.ListBuildings.get(this.GetBuildingCount()-1);
-			
-			this.ListMinions.remove(this.GetBuildingCount()-1);
-			
-			
-			return BuildingToSend;
-		}
+	}
 	
-
-
-
+	public Pieces PlaceBuilding()
+	{
+		Pieces BuildingToSend =  this.ListBuildings.get(this.GetBuildingCount()-1);
+		
+		this.ListMinions.remove(this.GetBuildingCount()-1);
+		
+		
+		return BuildingToSend;
+	}
 
 }
