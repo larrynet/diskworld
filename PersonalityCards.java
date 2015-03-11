cf9efd5917fb45937ae9c2444526adaab08c63a6
ghazan <gay.hazan@gmail.com>
2015-03-11 13:44:54 -0400

    Added ShowImage to person cards

--- PersonalityCards.java
+++ PersonalityCards.java
@@ -1,5 +1,15 @@
+import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.Serializable;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 /**
  * @author Niloufar
  *
@@ -30,6 +40,60 @@ public class PersonalityCards implements Cards, Serializable {
 	{
 		return this.Id;
 	}
+	
+	public void ShowImage(String _Title)
+	{
+		
+		//replace all space in Name
+		Name = Name.replace(" ", "");
+		String path = "/Users/gayhazan/Documents/workspace/LocDiskworld/src/Cards/GreenCards/" + Name+".jpg";
+		System.out.println("Looking for file in " + path);
+	    JFrame editorFrame = new JFrame(_Title);
+	    
+	    ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource("Cards/Personalities/" + Name + ".jpg").getFile());
+
+	    editorFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+	    BufferedImage image = null;
+
+	    try
+
+	    {
+	      image = ImageIO.read(file);
+
+	    }
+
+	    catch (Exception e)
+	    {
+	      e.printStackTrace();
+	      System.exit(1);
+	    }
+
+	    BufferedImage bi = (BufferedImage)image;
+	    Image Scaled = bi.getScaledInstance(300, 350, Image.SCALE_SMOOTH);
+	   
+	    JLabel jLabel = new JLabel();
+	    //jLabel.setIcon(imageIcon);
+	    
+	       
+	        
+	    jLabel.setIcon(new ImageIcon(Scaled));    
+	    
+	    
+	    editorFrame.getContentPane().add(jLabel, BorderLayout.CENTER);
+
+
+
+
+	    editorFrame.pack();
+
+	    editorFrame.setLocationRelativeTo(null);
+
+	    editorFrame.setVisible(true);
+
+	}
+
 	
 	public CardType GetCardType()
 	 {
